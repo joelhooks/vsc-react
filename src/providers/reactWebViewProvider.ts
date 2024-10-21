979bfc5fb5d517829d52f9b65205f1470e5a9fb3
joel <joelhooks@gmail.com>
2024-10-20 18:48:40 -0700

    basic web view with a mux player

--- src/providers/reactWebViewProvider.ts
+++ src/providers/reactWebViewProvider.ts
@@ -5,26 +5,35 @@ export class ReactWebViewProvider implements vscode.WebviewViewProvider {
 
   constructor(private readonly context: vscode.ExtensionContext) {}
 
-  public async resolveWebviewView(webviewView: vscode.WebviewView, _context: vscode.WebviewViewResolveContext, _token: vscode.CancellationToken) {
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    _context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
     this._view = webviewView
     webviewView.webview.options = {
       enableScripts: true,
     }
 
-    webviewView.webview.html = await this.getHtmlForWebview()
+    webviewView.webview.html = this.getHtmlForWebview()
   }
   
-   private async getHtmlForWebview() {
-      return `<!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Cat Coding</title>
-      </head>
-      <body>
-          <h1>🐱 Meow! 🐱</h1>
-      </body>
-      </html>`
-   }
+  private getHtmlForWebview() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cat Coding</title>
+        <script src="https://cdn.jsdelivr.net/npm/@mux/mux-player"></script>
+    </head>
+    <body>
+        <h1>🐱 Meow! 🐱</h1>
+        <mux-player
+            playback-id="EcHgOK9coz5K4rjSwOkoE7Y7O01201YMIC200RI6lNxnhs"
+            options='{"autoplay": true, "controls": true}'
+          ></mux-player>
+    </body>
+    </html>`
+  }
 }
